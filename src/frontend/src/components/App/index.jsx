1bbb11f01268498d98a70db79d85ca7d35e8b510
STARK <Starkov123123@gmail.com>
2020-06-12 19:06:33 +0300

    Основа фронта

---
+++ src/frontend/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+// React
+import React from 'react'
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                Пизда
+            </div>
+        )
+    }
+}
+
+export default App
