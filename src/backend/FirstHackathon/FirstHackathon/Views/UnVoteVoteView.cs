a25a577c13f7f47efce38f1dc359028c163c2b4a
DaKushmuhaetov <dank.9001@gmail.com>
2020-06-14 14:37:11 +0500

    Presentation

---
+++ src/backend/FirstHackathon/FirstHackathon/Views/UnVoteVoteView.cs
@@ -0,0 +1,14 @@
+﻿using FirstHackathon.Models.Votes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstHackathon.Views
+{
+    public sealed class UnVoteVoteView
+    {
+        public Guid Id { get; set; }
+        public VotingView VotingView { get; set; }
+    }
+}
